b76e6ac9e7ac5521dcf3fd6b6b5949c7951f4363
Jerel Crespo <jerel.crespo@gmail.com>
2016-12-19 00:06:33 -0500

    allowing cycle to turn, still no collision

--- source/app.js
+++ source/app.js
@@ -5,9 +5,7 @@ function initCanvas() { // anything i need to do on canvas goes in here
   let y = 50, x = 50;
   // let curDir = x++; // initial trajectory of poorman's hacky lightCycle
 
-
-  // variables to turn on event
-  let lastKey = null;
+  let lastKey = null; // setting initial trajectory, below
   $(document).keydown(function(e) {
     if (e.which == 87){
       console.log('pressed up');
@@ -25,37 +23,6 @@ function initCanvas() { // anything i need to do on canvas goes in here
   });
 
 /*
-  $(document).keydown(function(e) {
-    if (e.which === 87){
-      console.log('pressed up')
-      lastKey = turnUp;
-    } else return;
-  });
-
-  $(document).keydown(function(e) {
-    if (e.which === 65){
-      console.log('pressed left');
-      lastKey = turnLeft;
-    } else return;
-  });
-
-  $(document).keydown(function(e) {
-    if (e.which === 83){
-      console.log('pressed down');
-      lastKey = turnDown;
-    } else return;
-  });
-
-  $(document).keydown(function(e) {
-    if (e.which === 68){
-      console.log('pressed right');
-      lastKey = turnRight;
-    } else return;
-  });
-*/
-
-
-/*
 if W is pressed, lastKey = turnUp
 if A is pressed, lastKey = turnLeft
 if S is pressed, lastKey = turnDown
@@ -67,15 +34,12 @@ right: [39, 68], // right-arrow, D
 start_game: [13, 32] // enter, spacebar
 */
 
-
-
-  function animate(){ // firing rapidly, keep code slim
-    ctx.save(); //saves changes before refiring animate
-    /*ctx.clearRect(0, 0, cW, cH); //commenting this out, I want my tron to 'draw' where it drives*/
+  function animate(){
+    ctx.save();
     // draw below
     ctx.fillStyle = '#0079D3';
     let start = ctx.fillRect(x, y, 5, 4); //start @ xpixel/ypixel, draw 5pxwide/4pxhigh
-    /*draw above*/
+    // logic below
     let currentDir = lastKey;
     if (lastKey === null){
       x++;
@@ -88,13 +52,8 @@ start_game: [13, 32] // enter, spacebar
     } else if (lastKey === 68){
       x++;
     }
-    // if (x == 50){ // when game starts,
-    //   let moveX = 0; // set initial trajectory
-    // }
-
-    /*logic above*/
   }
-  const animateInterval = setInterval(animate, 30); //this is what is making the animate function fire so frequently (for smoother animation)
+  const animateInterval = setInterval(animate, 30);
 }
 
 window.addEventListener('load', function(event) {
@@ -103,23 +62,7 @@ window.addEventListener('load', function(event) {
 
 
 /*
-
-if (y == 50){ // change to if (keyevent) gonna be ignored cause not 50
-      shiftY = 0;
-      x++
-    } else if (x == 0) {let shiftY = y++;} hi
-
-// outside animate
-let x = 50, y = 50; //this will be starting position, outside of animate
-let curDir = x++; // initial trajectory of poorman's hacky lightCycle
-
-//inside animate
-ctx.fillRect(x, y, 5, 4);  // poorman's hacky lightCycle + lightWall
-if (up) {
-  console.log(up);
-}
-
-
+Pseudo for collision detection:
 
 
 
