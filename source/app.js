3712283624e5bfa8e1eae4370ee4baa59a56868e
Jerel Crespo <jerel.crespo@gmail.com>
2016-12-18 22:06:39 -0500

    adding pseudo. on the right track

--- source/app.js
+++ source/app.js
@@ -1,24 +1,24 @@
-/*attempt 4ish*/
-$(document).ready(function() {
-  $('input').keydown(function(e) {
-    if(e.keyCode === 87) {
-      console.log($(this).val());
-    }
-  });
-});
 function initCanvas() { // anything i need to do on canvas goes in here
   const ctx = document.getElementById('grid').getContext('2d'); /*756x480*/ // compresses the two variables context and canvas into one var.. will have to experiment to see how effective this works at my level
   const cW = ctx.canvas.width, cH = ctx.canvas.height; // easy access
-
+  // initial positioning
   let y = 50, x = 50;
-  let curDir = x++; // initial trajectory of poorman's hacky lightCycle
+  // let curDir = x++; // initial trajectory of poorman's hacky lightCycle
+
 
   // variables to turn on event
+  let lastKey = null;
   let turnUp = y--;
   let turnLeft = x--;
   let turnDown = y++;
   let turnRight = x++;
   // eventlisteners to trigger turn variables
+/*
+if W is pressed, lastKey = turnUp
+if A is pressed, lastKey = turnLeft
+if S is pressed, lastKey = turnDown
+if D is pressed, lastKey = turnRight
+*/
 
 
 
@@ -27,29 +27,26 @@ function initCanvas() { // anything i need to do on canvas goes in here
     /*ctx.clearRect(0, 0, cW, cH); //commenting this out, I want my tron to 'draw' where it drives*/
     // draw below
     ctx.fillStyle = '#0079D3';
-    ctx.fillRect(x, y, 5, 4); //start @ xpixel/ypixel, draw 5pxwide/4pxhigh
+    let start = ctx.fillRect(x, y, 5, 4); //start @ xpixel/ypixel, draw 5pxwide/4pxhigh
     /*draw above*/
-    /*logic below*/
-
-    // if (up) {
-    //   console.log(up);
+    if (lastKey === null){
+      x++;
+    } else if (lastKey === ){
+      let lastKey = turnUp;
+    }
+    // if (x == 50){ // when game starts,
+    //   let moveX = 0; // set initial trajectory
     // }
 
-/*
-if (y == 50){ // change to if (keyevent) gonna be ignored cause not 50
-      shiftY = 0;
-      x++
-    } else if (x == 0) {let shiftY = y++;}
-    */
+
+
 
     /*logic above*/
     ctx.restore; // may not need, returns last saved board state before clear
   }
   const animateInterval = setInterval(animate, 30); //this is what is making the animate function fire so frequently (for smoother animation)
 }
-//
 
-//
 window.addEventListener('load', function(event) {
   initCanvas();
 })
